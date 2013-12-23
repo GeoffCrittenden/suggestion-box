e82d55e1ab7d58b6ffead172e251a8cafb5db6e1
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-23 12:14:55 -0600

    adding Suggestion, User, Comment and Vote

---
+++ app/controllers/suggestions_controller.rb
@@ -0,0 +1,10 @@
+class SuggestionsController < ApplicationController
+  def index
+  end
+
+  def create
+    @title = params[:suggestion][:title]
+    @text = params[:suggestion][:text] 
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SuggestionBox::Application.routes.draw do
+  get "suggestions/index"
   get "suggestion/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
