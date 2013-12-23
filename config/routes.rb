4583ad436c876bd9c259d40b29cb63ff12e2d156
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-23 11:30:36 -0600

    added resources :suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ SuggestionBox::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :suggestions
 
   # Example resource route with options:
   #   resources :products do
