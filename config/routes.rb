4fdbd9383f546244457478269f8922e34af87a3d
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-23 16:43:29 -0600

    added suggestions list to suggestions/index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SuggestionBox::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'suggestion#index'
+  root 'suggestions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
